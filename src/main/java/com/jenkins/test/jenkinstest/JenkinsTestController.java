dd3b1cb7b091646db7ba86fc0e3a1fbee4135dba
neardybhaskar <neardybhaskar@gmail.com>
2021-09-01 00:06:36 +0530

    Added controller file

---
+++ src/main/java/com/jenkins/test/jenkinstest/JenkinsTestController.java
@@ -0,0 +1,18 @@
+package com.jenkins.test.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Bhaskar Singh
+ * @date 8/31/2021 10:51 PM
+ */
+@RestController
+public class JenkinsTestController {
+
+    @GetMapping("/jenkinsTest")
+    public String jenkinsTestCall() {
+        return "Welcome to Jenkins Test";
+    }
+
+}
